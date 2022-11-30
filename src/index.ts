42e72d87f4aff8b2c3880f5fc54855bac10faeac
getwildysy <42105084+getwildysy@users.noreply.github.com>
2022-11-30 11:39:04 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+</div>`;
